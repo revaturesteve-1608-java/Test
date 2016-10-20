7fca8718c1a0af176336d025a160c7f30bcb2175
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-10-20 06:38:19 -0700

    fix issue and comment
    fix the delete post with profile issue,
    fix the profile in associate,
    add comment to the dto package

--- Project3/src/main/java/project3/dto/AwsKey.java
+++ Project3/src/main/java/project3/dto/AwsKey.java
@@ -5,24 +5,45 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * The key to the S3
+ */
 @Entity
 @Table(name="AwsKey")
 public class AwsKey {
 
+	/**
+	 * The primary ID of the AwsKey
+	 */
 	@Id
 	@Column(name="aws_id")
 	private int id;
 	
+	/**
+	 * The access key to S3
+	 */
 	@Column(name="aws_key")
 	private String accessKey;
 	
+	/**
+	 * The secret access key to S3
+	 */
 	@Column(name="aws_pkey")
 	private String secretAccessKey;
 
+	/**
+	 * An empty constructor for initialize the AwsKey object.
+	 */
 	public AwsKey() {
 		
 	}
 
+	/**
+	 * A constructor for initialize the AwsKey object.
+	 * @param id The primary ID of the AwsKey
+	 * @param accessKey The access key to S3
+	 * @param secretAccessKey The secret access key to S3
+	 */
 	public AwsKey(int id, String accessKey, String secretAccessKey) {
 		super();
 		this.id = id;
@@ -30,30 +51,57 @@ public class AwsKey {
 		this.secretAccessKey = secretAccessKey;
 	}
 
+	/**
+	 * Getting the id of the AwsKey
+	 * @return The id of the AwsKey
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Set a new id for the AwsKey
+	 * @param id The new id to be set
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Getting the accessKey of the AwsKey
+	 * @return The accessKey of the AwsKey
+	 */
 	public String getAccessKey() {
 		return accessKey;
 	}
 
+	/**
+	 * Set a new accessKey for the AwsKey
+	 * @param accessKey The new accessKey to be set
+	 */
 	public void setAccessKey(String accessKey) {
 		this.accessKey = accessKey;
 	}
 
+	/**
+	 * Getting the secretAccessKey of the AwsKey
+	 * @return The secretAccessKey of the AwsKey
+	 */
 	public String getSecretAccessKey() {
 		return secretAccessKey;
 	}
 
+	/**
+	 * Set a new secretAccessKey for the AwsKey
+	 * @param secretAccessKey The new secretAccessKey to be set
+	 */
 	public void setSecretAccessKey(String secretAccessKey) {
 		this.secretAccessKey = secretAccessKey;
 	}
 
+	/**
+	 * A String representation of the object
+	 */
 	@Override
 	public String toString() {
 		return "AwsKey [id=" + id + ", accessKey=" + accessKey + ", secretAccessKey=" 
